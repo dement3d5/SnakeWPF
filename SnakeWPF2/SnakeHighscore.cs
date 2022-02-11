15548abacfe2528c1d8069fced243c7df190125c
Нурлан Аминов <alimovn125@gmail.com>
2022-02-11 19:51:16 +1000

    Добавьте файлы проекта.

---
+++ SnakeWPF2/SnakeHighscore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace SnakeWPF2
+{
+    public class SnakeHighscore
+    {
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ SnakeWPF2/SnakePart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace SnakeWPF2
+{
+    class SnakePart
+    {
+        public UIElement UiElement { get; set; }
+        public Point Position{ get; set; }
+        public bool IsHead{ get; set; }
+    }
+}
